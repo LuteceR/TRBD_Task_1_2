83bda2439e76ac585fc47ce1c3fd4481f8f878fc
LuteceR <93578660+LuteceR@users.noreply.github.com>
2025-09-28 00:10:21 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,26 +13,25 @@ namespace TRBD_Task_1_2
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
 
-            // объявление dataset'а
-            var ds = new ds();
+        // обновление данных таблицах
+        public void updateDataGridViews()
+        {
+            ds ds = new ds();
 
             try
             {
                 ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
-            } 
+            }
             catch
             {
                 ds.WriteXml("dataset.xml", XmlWriteMode.IgnoreSchema);
             }
-            
+
             // вывод БД на экран с переименованными столбцами
-            string[] namesForWorkers = new string[5] {"ФИО работника", "Дата рождения", "ИНН", "СНИЛС", "Паспортные данные"};
+            string[] namesForWorkers = new string[5] { "ФИО работника", "Дата рождения", "ИНН", "СНИЛС", "Паспортные данные" };
             dataGridView1.DataSource = ds.Worker.DefaultView.ToTable(false, "full_name", "date_of_birth", "INN", "SNILS", "pasport_data");
-            
+
             for (int i = 0; i < namesForWorkers.Length; i++)
             {
                 dataGridView1.Columns[i].HeaderText = namesForWorkers[i];
@@ -41,7 +40,10 @@ namespace TRBD_Task_1_2
             // заполнение оставшегося места столбцом "full_name"
             dataGridView1.Columns["full_name"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 
-            string[] namesForJobs = new string[3] {"Начало работы", "Конец работы", "Описание"};
+            // заполнение оставшегося места столбцом "full_name"
+            dataGridView1.Columns["full_name"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            string[] namesForJobs = new string[4] { "рабочий", "Начало работы", "Конец работы", "Описание" };
             dataGridView2.DataSource = ds.Job.DefaultView.ToTable(false, "ID_Worker", "start_date", "end_date", "description");
             dataGridView2.Columns["ID_Worker"].Visible = false;
 
@@ -50,6 +52,13 @@ namespace TRBD_Task_1_2
                 dataGridView2.Columns[i].HeaderText = namesForJobs[i];
             }
             dataGridView2.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            updateDataGridViews();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -63,50 +72,48 @@ namespace TRBD_Task_1_2
             addworker.ShowDialog();
 
             // обновление данных в datagridview1
-            var ds = new ds();
+            ds ds = new ds();
             ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
 
-            string[] namesForWorkers = new string[5] { "ФИО работника", "Дата рождения", "ИНН", "СНИЛС", "Паспортные данные" };
-            dataGridView1.DataSource = ds.Worker.DefaultView.ToTable(false, "full_name", "date_of_birth", "INN", "SNILS", "pasport_data");
-
-            for (int i = 0; i < namesForWorkers.Length; i++)
-            {
-                dataGridView1.Columns[i].HeaderText = namesForWorkers[i];
-            }
-
-            dataGridView1.Columns["full_name"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            updateDataGridViews();
         }
 
         private void editworkerbtn_Click(object sender, EventArgs e)
         {
-            DataGridViewCellCollection rows = dataGridView1.SelectedRows[0].Cells;
-            
-            EditWorker editworker = new EditWorker(sender, e, rows);
-            editworker.ShowDialog();
+            // проверка на существование выбранной строки
+            try
+            {
+                DataGridViewCellCollection rows = dataGridView1.SelectedRows[0].Cells;
 
+                EditWorker editworker = new EditWorker(sender, e, rows);
+                editworker.ShowDialog();
+            }
+            catch
+            {
+                MessageBox.Show("Не выбран работник");
+            }
 
             // обновление данных в datagridview1
             var ds = new ds();
             ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
-
-            string[] namesForWorkers = new string[5] { "ФИО работника", "Дата рождения", "ИНН", "СНИЛС", "Паспортные данные" };
-            dataGridView1.DataSource = ds.Worker.DefaultView.ToTable(false, "full_name", "date_of_birth", "INN", "SNILS", "pasport_data");
-
-            for (int i = 0; i < namesForWorkers.Length; i++)
-            {
-                dataGridView1.Columns[i].HeaderText = namesForWorkers[i];
-            }
-
-            dataGridView1.Columns["full_name"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            
+            updateDataGridViews();
         }
 
         private void deleteworkerbtn_Click(object sender, EventArgs e)
         {
-            var ds = new ds();
+            ds ds = new ds();
             ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
 
             DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
-            ds.Worker.Rows.RemoveAt(selectedRow.Index);
+            try
+            {
+                ds.Worker.Rows.RemoveAt(selectedRow.Index);
+            }
+            catch
+            {
+                MessageBox.Show("Нельзя удалить из-за ограничений на отношении");
+            }
 
             // обновление id
             // отключаю ограничения, чтобы можно было выставить id без ошибок об уникальности
@@ -123,17 +130,8 @@ namespace TRBD_Task_1_2
             ds.EnforceConstraints = true;
 
             ds.WriteXml("dataset.xml", XmlWriteMode.IgnoreSchema);
-            
-            // обновление данных в datagridview1
-            string[] namesForWorkers = new string[5] { "ФИО работника", "Дата рождения", "ИНН", "СНИЛС", "Паспортные данные" };
-            dataGridView1.DataSource = ds.Worker.DefaultView.ToTable(false, "full_name", "date_of_birth", "INN", "SNILS", "pasport_data");
 
-            for (int i = 0; i < namesForWorkers.Length; i++)
-            {
-                dataGridView1.Columns[i].HeaderText = namesForWorkers[i];
-            }
-
-            dataGridView1.Columns["full_name"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            updateDataGridViews();
         }
 
         private void addjobbtn_Click(object sender, EventArgs e)
@@ -141,39 +139,34 @@ namespace TRBD_Task_1_2
             AddJob addjob = new AddJob();
             addjob.ShowDialog();
 
-            // обновление данных в datagridview1
+            // обновление данных в datagridview2
             var ds = new ds();
+
+            updateDataGridViews();
+
             ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
-
-            string[] namesForJobs = new string[3] { "Начало работы", "Конец работы", "Описание" };
-            dataGridView2.DataSource = ds.Job.DefaultView.ToTable(false, "start_date", "end_date", "description");
-
-            for (int i = 0; i < namesForJobs.Length; i++)
-            {
-                dataGridView2.Columns[i].HeaderText = namesForJobs[i];
-            }
-            dataGridView2.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
         }
 
         private void editjobbtn_Click(object sender, EventArgs e)
         {
-            DataGridViewCellCollection rows = dataGridView2.SelectedRows[0].Cells;
-
-            EditJob editjob = new EditJob(sender, e, rows);
-            editjob.ShowDialog();
-
-            // обновление данных в datagridview1
-            var ds = new ds();
+            ds ds = new ds();
             ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
 
-            string[] namesForJobs = new string[3] { "Начало работы", "Конец работы", "Описание" };
-            dataGridView2.DataSource = ds.Job.DefaultView.ToTable(false, "start_date", "end_date", "description");
-
-            for (int i = 0; i < namesForJobs.Length; i++)
+            try
             {
-                dataGridView2.Columns[i].HeaderText = namesForJobs[i];
+                DataGridViewRow selectedRow = dataGridView2.SelectedRows[0];
+                DataRow row = ds.Job.Rows[selectedRow.Index];
+                EditJob editjob = new EditJob(sender, e, row);
+                editjob.ShowDialog();
             }
-            dataGridView2.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            catch
+            {
+                MessageBox.Show("Не выбрана работа");
+            }
+
+            updateDataGridViews();
+            //// обновление данных в datagridview2
+            //ds.ReadXml("dataset.xml", XmlReadMode.IgnoreSchema);
         }
 
         private void deletejobbtn_Click(object sender, EventArgs e)
@@ -200,14 +193,7 @@ namespace TRBD_Task_1_2
 
             ds.WriteXml("dataset.xml", XmlWriteMode.IgnoreSchema);
 
-            string[] namesForJobs = new string[3] { "Начало работы", "Конец работы", "Описание" };
-            dataGridView2.DataSource = ds.Job.DefaultView.ToTable(false, "start_date", "end_date", "description");
-
-            for (int i = 0; i < namesForJobs.Length; i++)
-            {
-                dataGridView2.Columns[i].HeaderText = namesForJobs[i];
-            }
-            dataGridView2.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            updateDataGridViews();
         }
     }
 }
